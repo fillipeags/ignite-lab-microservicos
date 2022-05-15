a082156036845f64f8bfd8dc87a8acb8b0511615
Fillipe Augusto <fillipeags@Fillipes-MacBook-Air.local>
2022-05-15 20:15:55 -0300

    feat: integrate frontend with backend

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,4 +1,6 @@
+import { ApolloProvider } from "@apollo/client"
 import { UserProvider } from "@auth0/nextjs-auth0"
+import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
